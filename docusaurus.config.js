d9dc8050b88c62ec8e496dced014332caa65ce54
Justin <fogestjv@gmail.com>
2024-08-27 00:10:07 +0000

    Switching to use the correct application ID for the algolia search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,56 +4,55 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Nuke Family Docs',
-  tagline: 'Documentation for the Nuke Family',
+  title: "Nuke Family Docs",
+  tagline: "Documentation for the Nuke Family",
 
   // Use the favicon.ico in the static/img directory
-  favicon: 'img/favicon.ico',
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://info.nuke.family',
+  url: "https://info.nuke.family",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Fog-Development/info.nuke.family/blob/master/',
+            "https://github.com/Fog-Development/info.nuke.family/blob/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Fog-Development/info.nuke.family/blob/master/',
+            "https://github.com/Fog-Development/info.nuke.family/blob/master/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -63,41 +62,41 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/og-image.png',
+      image: "img/og-image.png",
       navbar: {
-        title: 'Nuke Family Docs',
+        title: "Nuke Family Docs",
         logo: {
-          alt: 'Nuke Family Logo',
-          src: 'img/icon.svg',
+          alt: "Nuke Family Logo",
+          src: "img/icon.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/Fog-Development/info.nuke.family/',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/Fog-Development/info.nuke.family/",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Docs',
-                to: '/',
+                label: "Docs",
+                to: "/",
               },
             ],
           },
-    /*      {
+          /*      {
             title: 'Community',
             items: [
               {
@@ -136,34 +135,34 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: '5PI6B898KN',
-  
+        appId: "KUKZFX8AXP",
+
         // Public API key: it is safe to commit it
-        apiKey: '73387f5ffdd6f7db09ce5f987afac7e0',
-  
-        indexName: 'info-nuke',
-  
+        apiKey: "73387f5ffdd6f7db09ce5f987afac7e0",
+
+        indexName: "info-nuke",
+
         // Optional: see doc section below
         contextualSearch: true,
-  
+
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'external\\.com|domain\\.com',
-  
+
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
         //replaceSearchResultPathname: {
         //  from: '/docs/', // or as RegExp: /\/docs\//
         //  to: '/',
         //},
-  
+
         // Optional: Algolia search parameters
         searchParameters: {},
-  
+
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
-  
+        searchPagePath: "search",
+
         // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
         insights: false,
-  
+
         //... other Algolia params
       },
     }),
